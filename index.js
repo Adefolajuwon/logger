51d62acf1f9fb8847c0e0b685f2001ce4aeaf304
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-03-06 00:35:59 +0100

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+class RequestLogger {
+	constructor(opts = {}) {
+		this.opts = opts;
+	}
+	async logRequest(req, res, next) {
+		if (this.opts.enable == false) {
+			next();
+			return;
+		}
+	}
+}
